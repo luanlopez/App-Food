33518aa33231db72a5755f2f74d6e12cb4444158
Guilherme-A-Santos <guilhermealves3489@hotmail.com>
2021-02-12 20:12:42 -0300

    Dev: Creating product model, and controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,45 @@
+const ProductModel = require('../models/products.models')
+
+class ProductController {
+  async createProduct(req, res) {
+    try {
+      const createProduct = await ProductModel.create({
+        ...req.body
+      })
+
+      return res.status(201).json({ createProduct, message: 'Product created with success!'})
+    } catch (error) {
+      return res.status(400).json({ error, message: 'Product not created :('})
+    }
+  }
+
+  async getProducts(req, res) {
+    const products = await ProductModel.find()
+
+    return res.json({ products }).status(200)
+  }
+
+  async getProductById(req, res) {
+    const { id } = req.params
+
+    const product = await ProductModel.findById(id)
+
+    return res.json({ product }).status(200)
+  }
+
+  async deleteProduct(req, res) {
+    const { id } = req.params
+
+    const productFounded = await ProductModel.findById(id)
+
+    if (!productFounded) {
+      return res.status(404).json({ message: 'Product does not exist :('})
+    }
+
+    await ProductModel.deleteOne({ _id: id })
+
+    return res.status(204)
+  }
+}
+
+module.exports = new ProductController();
---
+++ src/models/products.models.js
@@ -0,0 +1,37 @@
+
+const { Schema, model} = require('mongoose')
+
+const ProductSchema = new Schema(
+    {
+      store_id: {
+        type: String, //Sujeito a mudança para number
+        required: true
+      },
+      seller_id: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      active: {
+        type: Boolean
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      },
+      updatedAt:  {
+        type: Date,
+        default: Date.now
+      }
+    }
+
+)
+
+module.exports = model("Product", ProductSchema)

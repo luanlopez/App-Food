9d10b44b80c9954a2095cce18d4f6e791d9e1cf6
Luan <luannlopessilva@outlook.com>
2021-02-11 21:16:56 -0300

    criando pastas para iniciar o desenvolvimento

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,20 @@
-const { Router } = require('express')
+const {
+  Router,
+  request
+} = require('express')
 
 const routes = new Router()
 
 routes.get('/test', (request, response) => response.json({
   message: "Luan pilantra",
-}
-).status(200))
+}).status(200));
+
+
+//ROTAS RE USUÁRIO
+routes.get('/users');
+routes.post('/users');
+routes.patch('/users/:user_id');
+routes.get('/users/:user_id');
+routes.delete('/users/:user_id');
 
 module.exports = routes

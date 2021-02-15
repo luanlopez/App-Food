1e1c770bc0b981c988d5e37381ec440616c6cf36
Luan <luannlopessilva@outlook.com>
2021-02-15 20:05:44 -0300

    Dev: create controllers of orders and testing

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -2,9 +2,9 @@ const OrderController = require('../controllers/order.controller');
 
 module.exports = (router) => {
   router.get('/orders', OrderController.getOrders);
-  router.get('/orders/:ordeId', OrderController.getOrderById);
-  router.get('/orders/:store_id', OrderController.getOrdersByStore);
-  router.get('/orders/:seller_id', OrderController.getOrdersBySeller);
+  router.get('/orders/:order_id', OrderController.getOrderById);
+  router.get('/orders/store/:store_id', OrderController.getOrdersByStore);
+  router.get('/orders/seller/:seller_id', OrderController.getOrdersBySeller);
   router.post('/orders', OrderController.createOrder);
   router.patch('/orders/:orderId', OrderController.cancelOrder);
   router.put('/orders/:orderId', OrderController.updateOrder);
